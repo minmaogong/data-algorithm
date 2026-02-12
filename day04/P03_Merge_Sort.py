ae1a7a16fc6c3ac5dd28b1ad39ae98a3b4811afe
公茂民 <maomin.gong@eeoa.com>
2026-02-12 11:50:24 +0800

    new: 归并排序

---
+++ day04/P03_Merge_Sort.py
@@ -0,0 +1,37 @@
+"""
+    归并排序
+"""
+
+# 对两个数组进行和平==>得到一个有序的数据
+def merge(left, right):
+    merged = []
+    i = j = 0
+    while i < len(left) and j < len(right):
+        if left[i] < right[j]:
+            merged.append(left[i])
+            i += 1
+        else:
+            merged.append(right[j])
+            j += 1
+
+    merged.extend(left[i:]) # i 后面剩余的元素添加到merged列表中
+    merged.extend(right[j:]) # j 后面剩余的元素添加到merged列表中
+    return merged
+
+
+def merge_sort(arr):
+    """归并排序"""
+    # 数组长度为1时，不再分割
+    if len(arr) <= 1:
+        return arr
+
+    # 获取切分的分割点
+    mid = len(arr) // 2
+
+    left = merge_sort(arr[:mid])
+    right = merge_sort(arr[mid:])
+
+    return merge(left, right)
+
+
+print(merge_sort([3, 1, 4, 2, 5]))
